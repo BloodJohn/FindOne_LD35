ad178506390920a7bd35b1d83da6561a1c1dfcfe
Eugene Pavlov <bjohn.ext@gmail.com>
2016-04-16 14:18:12 +0300

    add GameController

---
+++ Assets/Script/CardController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    public BoxCollider2D collider;
+    public GameObject sprite;
+    public bool upOrientation;
+    private Color color;
+
+    void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Cick!");
+
+        upOrientation = !upOrientation;
+        UpdateStats();
+        SetColor(8);
+    }
+
+    public void UpdateStats()
+    {
+        Debug.Assert(sprite!=null, "Sprite in null!");
+        if (sprite == null) return;
+
+        sprite.transform.localEulerAngles =  new Vector3(0f, 0f, upOrientation ? 0f : 180f);
+    }
+
+    public void SetColor(int maxColor)
+    {
+        int rndColor = Random.Range(0, maxColor < 8 ? maxColor : 8);
+
+        color = Color.white;
+
+        switch (rndColor)
+        {
+            case 1: color = Color.red; break;
+            case 2: color = Color.green; break;
+            case 3: color = Color.blue; break;
+            case 4: color = Color.yellow; break;
+            case 5: color = Color.cyan; break;
+            case 6: color = Color.magenta; break;
+            case 7: color = Color.grey; break;
+            case 8: color = Color.gray; break;
+            default: color = Color.white; break;
+        }
+
+        sprite.GetComponent<Renderer>().material.color = color;
+    }
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+    private List<CardController> cardList = new List<CardController>(10);
+
+    // Use this for initialization
+    void Start()
+    {
+        CardController[] allCard = FindObjectsOfType(typeof(CardController)) as CardController[];
+        foreach (CardController card in allCard)
+        {
+            cardList.Add(card);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
+            if (hitCollider != null)
+            {
+                foreach (var card in cardList)
+                    if (hitCollider == card.collider) card.OnClick();
+            }
+        }
+    }
+}
