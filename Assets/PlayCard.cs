be7477264da3cbd7f697594d20926845d515a781
Eugene Pavlov <bjohn.ext@gmail.com>
2016-04-16 10:48:32 +0300

    click collider

---
+++ Assets/PlayCard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayCard : MonoBehaviour
+{
+    private BoxCollider2D collider;
+
+    void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+                if (Input.GetMouseButtonDown(0))
+        {
+                    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
+                    if (hitCollider != null)
+            {
+                Debug.Log("I'm hitting " + hitCollider.name);
+
+                if (hitCollider == collider) OnClick();
+            }
+        }
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Cick!");
+    }
+}
